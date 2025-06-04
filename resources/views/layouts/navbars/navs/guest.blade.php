1c8f6782564b48905004b4f04435878e5e69a5ae
Hugh0st <hug0.4lej4ndr0@gmail.com>
2025-06-04 12:45:11 -0600

    Imagen de fondo en el main con barra de navegacion superior modificada

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute">
     <div class="container">
         <div class="navbar-wrapper">
-            <a class="navbar-brand" href="#pablo">{{ __('Light Bootstrap Dashboard Laravel') }}</a>
+            <a class="navbar-brand" href="#pablo">{{ __('Primera verision modificada (Hugo)') }}</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar burger-lines"></span>
                 <span class="navbar-toggler-bar burger-lines"></span>
@@ -12,17 +12,17 @@
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a href="{{ route('dashboard') }}" class="nav-link">
-                        <i class="nc-icon nc-chart-pie-35"></i> {{ __('Dashboard') }}
+                        <i class="nc-icon nc-chart-pie-35"></i> {{ __('Estadisticas') }}
                     </a>
                 </li>
                 <li class="nav-item @if($activePage == 'register') active @endif">
                     <a href="{{ route('register') }}" class="nav-link">
-                        <i class="nc-icon nc-badge"></i> {{ __('Register') }}
+                        <i class="nc-icon nc-badge"></i> {{ __('Registrar') }}
                     </a>
                 </li>
                 <li class="nav-item @if($activePage == 'login') active @endif">
                     <a href="{{ route('login') }}" class="nav-link">
-                        <i class="nc-icon nc-mobile"></i> {{ __('Login') }}
+                        <i class="nc-icon nc-mobile"></i> {{ __('Iniciar sesión') }}
                     </a>
                 </li>
             </ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts/app', ['activePage' => 'welcome', 'title' => 'Light Bootstrap Dashboard Laravel by Creative Tim & UPDIVISION'])
 
 @section('content')
-    <div class="full-page section-image" data-color="black" data-image="{{asset('light-bootstrap/img/full-screen-image-2.jpg')}}">
+    <div class="full-page section-image" data-color="black" data-image="{{asset('light-bootstrap/img/tiendita_de_la_esquina.jpg')}}">
         <div class="content">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-lg-7 col-md-8">
-                        <h1 class="text-white text-center">{{ __('Welcome to Light Bootstrap Dashboard FREE Laravel Live Preview.') }}</h1>
+                    <div class="col-lg-9 col-md-8">
+                        <h1 class="text-white text-center">{{ __('¡Bienvenido al sistema de ventas! ') }}</h1>
+                    </div>
+                    <div class="col-lg-9 col-md-8">
+                        <h1 class="text-white text-center">{{ __('Tu sistema de ventas favorito') }}</h1>
                     </div>
                 </div>
             </div>
